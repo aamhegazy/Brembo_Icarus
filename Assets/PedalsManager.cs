82db824d7d58fcc7ae49767f65b05f2b9273d8bd
Ahmed <aamhegazy@gmail.com>
2022-06-25 20:47:51 +0200

    Braking Logic Implemented

--- Assets/PedalsManager.cs
+++ Assets/PedalsManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System;
 
 public class PedalsManager : MonoBehaviour
 {
@@ -12,27 +13,77 @@ public class PedalsManager : MonoBehaviour
 
 
 
+
     [SerializeField]
     private float speed;
     [SerializeField]
     private bool isAccelerating;
-
+    private bool isBraking;
     // Start is called before the first frame update
     void Start()
     {
         isAccelerating = false;
+        isBraking = false;
         speed = 0;
         displayedSpeed.text = speed.ToString();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if (isAccelerating)
+        {
+            if (speed <= 50)
+            {
+                speedIncrement = 0.2f;
+            }
+            else if (speed > 50 && speed <= 100)
+            {
+                speedIncrement = 0.1f;
+            }
+            else if (speed > 100 && speed <= 150)
+            {
+                speedIncrement = 0.1f;
+            }
+            else
+            {
+                speedIncrement = 0;
+            }
+        }
+        if (isBraking)
+        {
+            if (speed <= 0)
+            {
+                speedIncrement = 0;
+            }
+            else if (speed > 0 && speed <= 100)
+            {
+                speedIncrement = 0.2f;
+            }
+            else if (speed > 100 && speed <= 150)
+            {
+                speedIncrement = 0.1f;
+            }
+            else
+            {
+                speedIncrement = 0.05f;
+            }
+        }
+        
+
         if (isAccelerating) 
         {
             speed += speedIncrement;
-            displayedSpeed.text = speed.ToString();
         }
+        if (isBraking)
+        {
+            speed -= speedIncrement;
+        }
+        
+        displayedSpeed.text = Math.Round(speed).ToString();
+
     }
 
 
@@ -44,6 +95,17 @@ public class PedalsManager : MonoBehaviour
     public void OnAcceleratingStopped()
     {
         isAccelerating = false;
+
+    }
+
+    public void OnBrakingStarted()
+    {
+        isBraking = true;
+    }
+
+    public void OnBrakingStopped()
+    {
+        isBraking = false;
 
     }
 }
